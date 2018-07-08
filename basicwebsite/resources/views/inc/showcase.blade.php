a88f7f0af09e861e9c4aa09f85b725da4b2aab97
curogihu <curoasg@gmail.com>
2018-07-08 09:19:06 +0900

    Finish lecture 8, Compiling Assets.
    To be blunt, it failed such as resources/assets/sass/_custom.scss. I
    will check this reason later.

---
+++ basicwebsite/resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+	<div class="container">
+		<h1>Welcome to my site</h1>
+		<p class="lead">Welcome to my brand new Laravel powered site. This site uses Laravel version 5.5.40</p>
+	</div>
+</div>
