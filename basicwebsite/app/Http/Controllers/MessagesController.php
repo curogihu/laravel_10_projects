e9f0ab1dfd6edc9073eac3177909e4e964842b0a
curogihu <curoasg@gmail.com>
2018-07-08 15:47:41 +0900

    Finish lecture 11, Model, Database and migration

--- basicwebsite/app/Http/Controllers/MessagesController.php
+++ basicwebsite/app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -14,6 +15,18 @@ class MessagesController extends Controller
     			,'email' => 'required'
     	]);
 
-    	return 'Success';
+    	// return 'Success';
+
+    	// create new message
+    	$message = new Message;
+    	$message->name = $request->input('name');
+    	$message->email = $request->input('email');
+    	$message->message = $request->input('message');
+
+    	// save message
+    	$message->save();
+
+    	// redirect
+    	return redirect('/')->with('success', 'Message sent');
     }
 }
