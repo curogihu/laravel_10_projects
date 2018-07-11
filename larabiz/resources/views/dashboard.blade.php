b2ecdb9436fa71222d1802290a0187bff57597b3
curogihu <curoasg@gmail.com>
2018-07-11 11:08:42 +0900

    削除機能の追加

--- larabiz/resources/views/dashboard.blade.php
+++ larabiz/resources/views/dashboard.blade.php
@@ -15,13 +15,18 @@
                 @if(count($listings))
                     <table class="table table-striped">
                         <tr>
-                            <th>Company</th>
-                            <th></th>
+                            <th colspan="3">Company</th>
                         </tr>
                         @foreach($listings as $listing)
                             <tr>
                                 <td>{{ $listing->name }}</td>
                                 <td><a class="pull-right btn btn-default" href="/listings/{{ $listing->id }}/edit">Edit</a></td>
+                                <td>
+                                    {!! Form::open(['action' => ['ListingsController@destroy', $listing->id], 'method' => 'POST', 'class' => 'pull-left', 'onsubmit' => 'return confirm("Are you sure?")']) !!}
+                                        {{ Form::hidden('_method', 'DELETE') }}
+                                        {{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+                                      {!! Form::close() !!}
+                                </td>
                             </tr>
                         @endforeach
                     </table>
