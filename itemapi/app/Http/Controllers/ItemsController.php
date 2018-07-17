0b5c5372d7d30fc34e6328e39f496a5ed63ced2c
curogihu <curoasg@gmail.com>
2018-07-17 13:26:44 +0900

    更新用＋削除用のAPI作成、不要の関数を削除

--- itemapi/app/Http/Controllers/ItemsController.php
+++ itemapi/app/Http/Controllers/ItemsController.php
@@ -21,16 +21,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -50,7 +40,7 @@ class ItemsController extends Controller
         if($validator->fails()) {
             $response = array('response' => $validator->messages(), 'success' => false);
             return $response;
-        
+
         } else {
             $item = new Item;
             $item->text = $request->input('text');
@@ -76,17 +66,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -95,7 +74,22 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'text' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+
+        } else {
+            $item = Item::find($id);
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+
+            return response()->json($item);
+        }
     }
 
     /**
@@ -106,6 +100,10 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::find($id);
+        $item->delete();
+
+        $response = array('response' => 'Item deleted', 'success' => true);
+        return $response;
     }
 }
