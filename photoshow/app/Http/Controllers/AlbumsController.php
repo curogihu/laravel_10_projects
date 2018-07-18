7427380e1e36492538ebeab313a38c120585ae8f
curogihu <curoasg@gmail.com>
2018-07-18 18:13:07 +0900

    画像のアップロード機能の追加
    コマンド php artisan storage:linkを実行後でないと、
    直接http://127.0.0.1:8000/storage/album_covers/test.pngへアクセスしても404扱いになってしま
    う。

--- photoshow/app/Http/Controllers/AlbumsController.php
+++ photoshow/app/Http/Controllers/AlbumsController.php
@@ -15,6 +15,25 @@ class AlbumsController extends Controller
 	}
 
 	public function store(Request $request) {
-		return 123;
+		$this->validate($request, [
+			'name' => 'required',
+			'cover_image' => 'image|max:1999'
+		]);
+
+		// show file name with file extension
+		// return $request->file('cover_image')->getClientOriginalName();
+		$filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+
+		// get just the filename
+		$filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+
+		// get just the file extension
+		$extension = $request->file('cover_image')->getClientOriginalExtension();
+
+		$filenameToStore = $filename . '_' . time() . '.' . $extension;
+
+		$path = $request->file('cover_image')->storeAs('public/album_covers', $filenameToStore);
+
+		return $path;
 	}
 }
