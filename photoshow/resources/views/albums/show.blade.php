4b9f2ab4544cd855f41df7f574777159d75259ec
curogihu <curoasg@gmail.com>
2018-07-18 21:19:33 +0900

    各アルバムに紐付いた画像をすべて出力させる

--- photoshow/resources/views/albums/show.blade.php
+++ photoshow/resources/views/albums/show.blade.php
@@ -5,4 +5,33 @@
 	<a class="button secondary" href="/">Go Back</a>
 	<a class="button" href="/photos/create/{{ $album->id }}">Upload Photo to Album</a>
 	<hr>
+	@if(count($album->photos) <= 0)
+		<p>No Photos To Display</p>
+
+	@else
+		<?php
+			$colcount = count($album->photos);
+			$i = 1;
+		?>
+		<div id="photos">
+			<div class="row text-center">
+				@foreach($album->photos as $photo)
+					<div class="medium-4 columns <?php echo $i == $colcount ? ' end' : null ; ?>">
+						<a href="/photos/{{ $photo->id }}">
+							<img class="thumbnail" src="/storage/photos/{{ $photo->album_id }}/{{ $photo->photo }}" alt="{{ $photo->title }}">
+						</a>
+						<br>
+						<h4>{{ $photo->title }}</h4>
+					</div>
+
+					@if($i % 3 == 0)
+						<div class="row text-center">
+					@endif
+
+					<?php $i++; ?>
+
+				@endforeach
+			</div>
+		</div>
+	@endif
 @endsection
