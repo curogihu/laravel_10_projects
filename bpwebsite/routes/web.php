57bc46ba3dfd5a876e66b62c2a24cb1e285259c9
curogihu <curoasg@gmail.com>
2018-07-18 13:19:57 +0900

    Customer CRUDの作成
    https://github.com/laracasts/Laravel-5-Generators-Extended
    
    php artisan make:migration:schema create_customers_table --model=0
    php artisan backpack:crud customer

--- bpwebsite/routes/web.php
+++ bpwebsite/routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// admin group
+// http://127.0.0.1:8000/admin/customer
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function() {
+	// app/Http/Controllers/Admin/CustomerCrudController.php
+	CRUD::resource('customer', 'Admin\CustomerCrudController');
+});
