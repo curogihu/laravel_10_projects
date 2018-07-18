5641ffd8b962daa760b99e115c36c574495dece6
curogihu <curoasg@gmail.com>
2018-07-18 17:46:48 +0900

    フォームのコンポーネント設定＋ビューの作成
    Foundation, LaravelCollectiveを追加
    
    なお、LaravelCollective追加用のコマンド＋設定追加後に次のコマンドを実行　php artisan make:provider
    FormService

--- photoshow/config/app.php
+++ photoshow/config/app.php
@@ -164,6 +164,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -177,6 +179,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\FormServiceProvider::class,
     ],
 
     /*
@@ -226,6 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
---
+++ photoshow/resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PhotoShow</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.css">
+</head>
+<body>
+	<div class="row">
+		@yield('content')
+	</div>
+</body>
+</html>
