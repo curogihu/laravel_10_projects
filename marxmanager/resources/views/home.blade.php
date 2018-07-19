119787aa6c13cf7b38e36e05202dd6dcb54e7b41
curogihu <curoasg@gmail.com>
2018-07-19 09:54:57 +0900

    bootstrapからコピペ
    https://getbootstrap.com/docs/3.3/javascript/#modals

--- marxmanager/resources/views/home.blade.php
+++ marxmanager/resources/views/home.blade.php
@@ -8,16 +8,32 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <button class="btn btn-primary btn-lg"
+                            data-toggle="modal"
+                            data-target="#addModal"
+                            type="button"
+                            name="button">Add Bookmark</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 @endsection
