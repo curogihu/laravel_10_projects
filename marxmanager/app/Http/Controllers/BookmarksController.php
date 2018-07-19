33ba5b70ab0676b15fdba819daee8d9f924f5d1f
curogihu <curoasg@gmail.com>
2018-07-19 10:49:10 +0900

    DBへブックマーク保存の機能追加

--- marxmanager/app/Http/Controllers/BookmarksController.php
+++ marxmanager/app/Http/Controllers/BookmarksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Bookmark;
 
 class BookmarksController extends Controller
 {
@@ -20,5 +21,16 @@ class BookmarksController extends Controller
     		'name' => 'required',
     		'url' => 'required'
     	]);
+
+    	// create bookmark
+    	$bookmark = new Bookmark;
+    	$bookmark->name = $request->input('name');
+    	$bookmark->url = $request->input('url');
+    	$bookmark->description = $request->input('description');
+    	$bookmark->user_id = auth()->user()->id;	// ログインしたユーザーID
+
+    	$bookmark->save();
+
+    	return redirect('/home')->with('success', 'Bookmark Added');
     }
 }
